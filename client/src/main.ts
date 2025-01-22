940e33607c7304feb7c29c2f25c46a15dbec2137
wajisan <ubuntu@wajitech.host>
2025-01-22 11:36:32 +0000

    edit conf

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,6 +3,6 @@ import './style.css'
 import App from './App.vue'
 
 const app = createApp(App)
-app.config.globalProperties.$server_url = "https://fullboard.wajitech.fr:8998" as string
+app.config.globalProperties.$server_url = "https://fullboard.wajitech.fr" as string //:PORT
 //app.config.globalProperties.$server_url = "https://wajihome.wajitech.fr:9856" as string
 app.mount('#app');
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
-    host: 'wajihome.wajitech.fr',
+    host: '0.0.0.0',
     port: 9658
   }
 })
